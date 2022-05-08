be580712f4dde5fa6ed4feb47a6c36c93c429179
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-08 21:05:39 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <a
           className="App-link" 
           href="https://reactjs.org"
-          target="_blank"
+          target="_blank" 
           rel="noopener noreferrer"
         >
           Learn React
