6cea69307b27a39b1979d4a7c0e82115c53bc2c6
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-11 17:15:54 +0900

    [헤더] 로그아웃 이벤트 추가

--- src/comp/Header.tsx
+++ src/comp/Header.tsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import mc from '../core/Mc';
+import { setValue } from '../store/Data';
 
 const Header = () => {
+    const currIsLogin = mc.getValue('isLogin');
+    const [isLogin, setIsLogin] = useState(currIsLogin);
+
     return(
         <Navbar bg="light" expand="lg">
             <Container fluid>
                 <Navbar.Brand href="Main">Matching</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                <Nav
-                    className="me-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
-                >
-                    <Nav.Link href="Register">Register</Nav.Link>
-                    <Nav.Link href="#action2">Link</Nav.Link>
-                    <NavDropdown title="Link" id="navbarScrollingDropdown">
-                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action5">
-                        Something else here
-                    </NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="#" disabled>
-                    Link
-                    </Nav.Link>
-                </Nav>
-                <Form className="d-flex">
-                    <FormControl
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                    />
-                    <Button variant="outline-success">Search</Button>
-                </Form>
+                    <Nav
+                        className="me-auto my-2 my-lg-0"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                        {isLogin ? <Nav.Link onClick={() => {
+                            mc.store.dispatch(setValue({isLogin: false}));
+                            setIsLogin(false);
+                            document.location.href = '/Main' // 추후 삭제 
+                        }}>Logout</Nav.Link> 
+                        : <Nav.Link href="Login">Login</Nav.Link>}
+
+                        <Nav.Link href="Register">Register</Nav.Link>
+                        <NavDropdown title="Link" id="navbarScrollingDropdown">
+                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                            Something else here
+                        </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#" disabled>
+                        Link
+                        </Nav.Link>
+                    </Nav>
+
+                    <Form className="d-flex">
+                        <FormControl
+                            type="search"
+                            placeholder="Search"
+                            className="me-2"
+                            aria-label="Search"
+                        />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
