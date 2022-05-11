f47ac959ebc16ee83e055155435e2122fe105b77
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-11 16:31:35 +0900

    [스타일] index.css 파일에 스타일 추가

--- src/index.tsx
+++ src/index.tsx
@@ -16,6 +16,7 @@ import { parseCommandLine } from 'typescript';
 import queryString from "query-string";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
