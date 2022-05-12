24e00a588effe5bb5e88fdbaefdeebdb9957c7fa
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-12 17:02:53 +0900

    [유틸] 모바일 여부 함수 추가

--- src/core/Utils.ts
+++ src/core/Utils.ts
@@ -1,5 +1,14 @@
 class Utils {
+    isMobile(): boolean {
+        const user = navigator.userAgent;
+        let isCheck = false;
 
+        if ( user.indexOf("iPhone") > -1 || user.indexOf("Android") > -1 ) {
+            isCheck = true;
+        }
+
+        return isCheck;
+    }
 }
 
-export default Utils;
+export default Utils.prototype;
