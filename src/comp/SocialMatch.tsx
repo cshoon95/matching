9267153658c3e00ea2dce1d4eb68b0e01ccbf501
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-13 14:43:51 +0900

    [소셜 매치] carousel 까지 작업중

--- src/comp/SocialMatch.tsx
+++ src/comp/SocialMatch.tsx
@@ -1,34 +1,42 @@
 import React, { useState } from 'react';
-import { Carousel, } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import carousel1 from '../img/carousel1.jpeg';
 import carousel2 from '../img/carousel2.jpeg';
 import carousel3 from '../img/carousel3.jpeg';
 
-const SocialMatch = () => {
+const CarouselPanel = () => {
     return (
         <Carousel fade>
             <Carousel.Item>
                 <img
-                className="d-block w-100 h-200"
-                src={carousel1}
-                alt="First slide"
+                    className="d-block w-100 h-200"
+                    src={carousel1}
+                    alt="First slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className="d-block w-100 h-200"
-                src={carousel2}
-                alt="Second slide"
+                    className="d-block w-100 h-200"
+                    src={carousel2}
+                    alt="Second slide"
                 />
             </Carousel.Item>
             <Carousel.Item>
                 <img
-                className="d-block w-100 h-200"
-                src={carousel3}
-                alt="Third slide"
+                    className="d-block w-100 h-200"
+                    src={carousel3}
+                    alt="Third slide"
                 />
             </Carousel.Item>
         </Carousel>
+    )
+}
+
+const SocialMatch = () => {
+    return (
+        <>
+            <CarouselPanel/>
+        </>
     )
 };
 
