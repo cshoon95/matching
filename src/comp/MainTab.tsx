c481b3f7a90323178feb22a4d5c1f53a624e4553
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-14 18:15:37 +0900

    [구장예약] 구장예약 탭 패널 추가

--- src/comp/MainTab.tsx
+++ src/comp/MainTab.tsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import { Tabs, Tab, } from 'react-bootstrap';
 import SocialMatch from './SocialMatch';
+import StadiumReserve from './StadiumReserve';
 
 const MainTab = () => {
     const [key, setKey] = useState('socialMacth');
-
+    const onClick = (e: any) => {
+        debugger
+    }
     return (
         <Tabs
             id="controlled-tab-example"
             activeKey={key}
-            onSelect={(k) => setKey(key)}
+            onSelect={(selectedKey: string | null) => { if (selectedKey) setKey(selectedKey); }}
             className="mb-3"
         >
             <Tab eventKey="socialMacth" title="소셜 매치">
                 <SocialMatch />
             </Tab>
-            <Tab eventKey="???" title="미정">
-                
+            <Tab eventKey="stadiumReserve" title="구장 예약">
+                <StadiumReserve />
             </Tab>
             <Tab eventKey="???" title="미정">
                 
---
+++ src/comp/StadiumReserve.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StadiumReserve = () => {
+    return(
+        <div>구장예약을 한걸 소셜매치 탭에 나타날 수 있도록 슬슬 서버 구현 필요</div>
+    );
+}
+
+export default StadiumReserve;
