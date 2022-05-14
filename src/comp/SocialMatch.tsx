79ee6e40ca0d0109ce20dd9a79323a79495f4762
cshoon95 <55683499+cshoon95@users.noreply.github.com>
2022-05-14 17:59:53 +0900

    [소셜매치] 캘린더 탭 추가

--- src/comp/SocialMatch.tsx
+++ src/comp/SocialMatch.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Carousel from './Carousel';
-
+import CalendarTab from './CalendarTab';
 
 const SocialMatch = () => {
     return (
         <>
             <Carousel/>
+            <CalendarTab />
         </>
     )
 };
---
+++ src/comp/TabPanel.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { Tabs, Tab, } from 'react-bootstrap';
+import SocialMatch from './SocialMatch';
+
+const TabPanel = () => {
+    const [key, setKey] = useState('socialMacth');
+
+    return (
+        <Tabs
+            id="controlled-tab-example"
+            activeKey={key}
+            onSelect={(k) => setKey(key)}
+            className="mb-3"
+        >
+            <Tab eventKey="socialMacth" title="소셜 매치">
+                <SocialMatch />
+            </Tab>
+            <Tab eventKey="???" title="미정">
+                
+            </Tab>
+            <Tab eventKey="???" title="미정">
+                
+            </Tab>
+        </Tabs>
+    )
+}
+
+export default TabPanel;
